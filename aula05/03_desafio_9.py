919d99dd53b6d9bd4cd1b57a9d71e9b55285125f
miachafer <miachafer@gmail.com>
2022-05-17 20:07:30 -0300

    Adiciona solução de desafio 9

--- aula05/03_desafio_9.py
+++ aula05/03_desafio_9.py
@@ -44,4 +44,25 @@ The only thing I'll ever ask of you
 You've got to promise not to stop when I say when
 """
 
+# Tirando as quebras de linha sem usar o splitlines
+letra_sem_quebra_de_linha = everlong_foo_fighters.replace('\n', ' ')
 
+# Tirando as vírgulas
+letra_sem_virgula = letra_sem_quebra_de_linha.replace(', ', ' ')
+
+# Finalmente transformando as palavras da música em uma lista de palavras
+palavras = letra_sem_virgula.split(' ')
+
+# Criando um set com as palavras - trata-se aqui de um conjunto de palavras onde nenhuma delas se repete
+conjunto = set(palavra for palavra in palavras)
+
+# Transformando o set em uma lista novamente para ordená-la com o método sort()
+palavras_unicas = list(conjunto)
+
+# Ordenando a lista usando o método sort()
+palavras_unicas.sort()
+
+# Tranformando a lista em uma string novamente
+string_de_palavras = ' '.join(item for item in palavras_unicas)
+
+print(string_de_palavras) # Output:  And Breathe Come Down Everlong Hello Hold I I'll I'm I've If Out She Slow So The Tonight When You've again along always and anything ask away be been breathe can could down ever everything feel for forever good got head her here how in into it know me my myself not now of only out over promise real red sang say she sing stop the thing this throw to waited wanted waste when with wonder you you've your

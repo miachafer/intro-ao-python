8e23015d2b71741bd6e576cfc5406ac905f6a43f
miachafer <miachafer@gmail.com>
2022-05-16 21:04:09 -0300

    Adiciona solucao do desafio 5

--- aula03/03_desafio_5.py
+++ aula03/03_desafio_5.py
@@ -1,4 +1,12 @@
 # Crie uma lista com 60 números começando com o valor 1 e indo até o número 60 (ou seja, o número 60 deve estar na lista).
 
+lista = [i for i in range(1, 61)]
+
+print(lista)
 
 # Imprima todos os itens da sua lista de índice par. Imprima o índice e o item.
+print('índice | item')
+print('------ | ----')
+for i, item in enumerate(lista):
+    if i%2 == 0:
+        print(f'  {i}  | {item}')

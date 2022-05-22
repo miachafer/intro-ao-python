66c62684b2de0b8032489aaca345c37ef5f532d7
miachafer <miachafer@gmail.com>
2022-05-22 20:40:17 -0300

    Adiciona solução do desafio 7

--- aula04/03_desafio_7.py
+++ aula04/03_desafio_7.py
@@ -2,19 +2,22 @@
 ## No entanto, ele não está funcionando bem. Você pode consertá-lo?
 
 def calcular_media(valores):
-    tamanho = 1
+    tamanho = 0
     soma = 0.0
     for i, valor in enumerate(valores):
         soma += valor
-        i += 1
+        tamanho += 1
     media = soma / tamanho
+    return media
 
 continuar = True
 valores = []
 while continuar:
     valor = input('Digite um número para entrar na sua média ou "ok" para calcular o valor:')
+    if valor.lower() != 'ok':
+        valores.append(int(valor))
     if valor.lower() == 'ok':
-        continuar = false
+        continuar = False
 
 media = calcular_media(valores)
 print('A média calculada para os valores {} foi de {}'.format(valores, media))

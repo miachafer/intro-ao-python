0f9cbb88138ea2e07f1dcd2986d627d4670cc81e
miachafer <miachafer@gmail.com>
2022-05-22 22:06:02 -0300

    Adiciona solução do desafio 11 e arquivo de saída

--- aula06/04_desafio_11.py
+++ aula06/04_desafio_11.py
@@ -3,4 +3,25 @@
 # Exemplo de entrada: Oi mulheres maravilhosas do curso de Python do ConstruDelas!
 # Exemplo de saída: !saleDurtsnoC od nohtyP ed\nosruc od sasohlivaram serehlum iO
 
-arquivo = 'entrada_desafio_11.txt'
+arquivo1 = 'entrada_desafio_11.txt'
+
+# Abrindo o arquivo1 ('entrada_desafio_11.txt') no modo de leitura
+arquivo1_aberto = open(arquivo1, 'r')
+
+# Guardando na variável leitura o conteúdo lido do arquivo1
+leitura = arquivo1_aberto.read()
+
+# Criando arquivo2 para escrever o conteúdo invertido do arquivo1
+arquivo2 = 'saida_desafio_11.txt'
+
+# Abrindo o arquivo2 recém criado
+arquivo2_aberto = open(arquivo2, 'w')
+
+# Escrevendo no arquivo2 o conteúdo invertido do arquivo1
+arquivo2_aberto.write(leitura[::-1]) # Verificar arquivo 'saida_desafio_11.txt' e seu conteúdo: "!saleDurtsnoC od nohtyP ed osruc od sasohlivaram serehlum iO"
+
+# Fechando o arquivo2
+arquivo2_aberto.close()
+
+# Fechando o arquivo1
+arquivo1_aberto.close()

8208d67f0496aa344ed2546f6afd2a96b7117578
miachafer <miachafer@gmail.com>
2022-05-24 17:42:48 -0300

    Adiciona solução do desafio 13

--- aula07/03_desafio_13.py
+++ aula07/03_desafio_13.py
@@ -2,10 +2,17 @@ import time
 
 # Crie um decorator que calcule o tempo de execução de uma determinada função
 
-
+def timeofexecution(func):
+    def wrapper(x, y):
+        tempo_inicial = time.time()
+        func(x, y)
+        tempo_execucao = time.time() - tempo_inicial
+        print(f"Levou {tempo_execucao} segundo.")
+    return wrapper
 
 # Aplique seu decorator na função abaixo e veja quanto tempo a busca de um mesmo valor em um set e uma lista demoram para serem executadas
 
+@timeofexecution
 def encontrar_item(container, item):
     return item in container
 
@@ -14,8 +21,8 @@ def main():
     set_grande = set(range(tamanho))
     lista_grande = list(range(tamanho))
     item = 500399
-    encontrar_item(set_grande, item)
-    encontrar_item(lista_grande, item)
+    encontrar_item(set_grande, item) # Output: Levou 0.0 segundo.
+    encontrar_item(lista_grande, item) # Output: Levou 0.0015559196472167969 segundo.
 
 if __name__ == '__main__':
     main()
